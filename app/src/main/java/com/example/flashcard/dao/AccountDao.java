673bfc2d317814625d33d255551a45946b891551
dacdung <dungndhe163971@fpt.edu.vn>
2023-07-08 16:26:26 +0700

    Initial Commit

--- app/src/main/java/com/example/flashcard/dao/AccountDao.java
+++ app/src/main/java/com/example/flashcard/dao/AccountDao.java
@@ -1,7 +1,31 @@
 package com.example.flashcard.dao;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.flashcard.model.Account;
+
+import java.util.List;
 
 @Dao
 public interface AccountDao {
+    @Insert
+    void Insert(Account...accounts);
+    @Delete
+    void Delete(Account account);
+    @Update
+    void Update(Account account);
+
+    @Query("SELECT * FROM account")
+    List<Account> GetAccounts();
+
+
+    @Query("SELECT * FROM account WHERE accountName = :accountName AND accountPassword = :pass   ")
+    Account GetAccount(String accountName, String pass);
+
+
+
 }
